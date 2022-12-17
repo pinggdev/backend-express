81532477f44bde31768f1394ae7227ca867ff693
Kevin Sinaga <kevinsinaga11.ks@gmail.com>
2022-12-17 19:50:41 +0700

    delete bank

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -129,7 +129,24 @@ module.exports = {
         await bank.save();
         req.flash("alertMessage", "Success Update Bank");
         req.flash("alertStatus", "success");
+        res.redirect("/admin/bank");
       }
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/admin/bank");
+    }
+  },
+
+  deleteBank: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const bank = await Bank.findOne({ _id: id });
+      await fs.unlink(path.join(`public/${bank.imageUrl}`));
+      await bank.remove();
+      req.flash("alertMessage", "Success Delete Bank");
+      req.flash("alertStatus", "success");
+      res.redirect("/admin/bank");
     } catch (error) {
       req.flash("alertMessage", `${error.message}`);
       req.flash("alertStatus", "danger");
